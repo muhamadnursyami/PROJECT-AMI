6d8705ed9a601d257c6889cfee93a552d8b49294
Muhamad Nur Syami <nursami385@gmail.com>
2024-05-05 21:32:41 +0700

    feat : register, login, filterController, Dashboard per Role

---
+++ app/Filters/AuditorFilter.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Filters;
+
+use CodeIgniter\Filters\FilterInterface;
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class AuditorFilter implements FilterInterface
+{
+    public function before(RequestInterface $request, $arguments = null)
+    {
+        // sudah dijelaskan di AdminFilter
+        if (!session()->get('logged_in')) {
+            session()->setFlashdata('pesan', 'Anda belum Login !');
+            session()->setFlashdata('alert_type', 'danger');
+            return redirect()->to('/');
+        }
+
+        if (session()->get('role_id') != 'auditor') {
+            session()->setFlashdata('pesan', 'Anda belum Login !');
+            session()->setFlashdata('alert_type', 'danger');
+            return redirect()->to('/');
+        }
+    }
+    public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
+    {
+        // Do something here
+    }
+}
---
+++ app/Filters/PimpinanFilter.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Filters;
+
+use CodeIgniter\Filters\FilterInterface;
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class PimpinanFilter implements FilterInterface
+{
+    public function before(RequestInterface $request, $arguments = null)
+    {
+        // sudah dijelaskan di AdminFilter
+        if (!session()->get('logged_in')) {
+            session()->setFlashdata('pesan', 'Anda belum Login !');
+            session()->setFlashdata('alert_type', 'danger');
+            return redirect()->to('/');
+        }
+
+
+        if (session()->get('role_id') != 'pimpinan') {
+            session()->setFlashdata('pesan', 'Anda belum Login !');
+            session()->setFlashdata('alert_type', 'danger');
+            return redirect()->to('/');
+        }
+    }
+    public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
+    {
+        // Do something here
+    }
+}
