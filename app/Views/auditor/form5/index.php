fa1cda95aa9350db808262e10e503bb37ef6be75
Mahadi Dwi Nugraha <mahadidwinugraha@gmail.com>
2024-08-27 16:32:21 +0700

    fix form 5 auditor view

--- app/Views/auditor/form5/index.php
+++ app/Views/auditor/form5/index.php
@@ -75,62 +75,67 @@
                         <?php $uniqueKey = $value['kode_kriteria'] . '-' . $value['id']; ?>
                         <div id="<?= $uniqueKey ?>" class="hidden form-temuan">
                             <p>Kode kriteria - <?= $value['kode_kriteria'] ?></p>
-                            <form class="row text-center" action="/auditor/form-5/<?= $uuid2 ?>" method="post">
-                                <?= csrf_field() ?>
-                                <input type="text" name="id_ringkasan_temuan" value="<?= $value['id'] ?>" hidden>
-                                <input type="text" name="penjaminMutuAudit" value="<?= $penjaminMutuAudit ?>" hidden>
-                                <input type="text" name="pimpinanAuditi" value="<?= $wakil_auditi ?>" hidden>
 
-                                <div class="col-12 col-lg-6 form-group">
-                                    <label for="deskripsiTemuan">Deskripsi Temuan</label>
-                                    <textarea class="form-control" id="deskripsiTemuan-visible-<?= $uniqueKey ?>" rows="3" disabled></textarea>
-                                    <textarea class="form-control hidden-textarea" id="deskripsiTemuan-<?= $uniqueKey ?>" name="deskripsiTemuan" rows="3" style="display: none;"></textarea>
-                                </div>
-                                <div class="col-12 col-lg-6 form-group">
-                                    <label for="kriteria">Kriteria</label>
-                                    <div class="form-control-static" id="kriteria-visible-<?= $uniqueKey ?>" style="height: auto; padding: 10px; background: #f1f1f1; border-radius: 5px;"></div>
-                                    <textarea class="form-control hidden-textarea" id="kriteria-<?= $uniqueKey ?>" name="kriteria" rows="3" style="display: none;"></textarea>
-                                </div>
-                                <div class="col-12 col-lg-6 form-group">
-                                    <label for="akibat">Akibat</label>
-                                    <textarea class="form-control" id="akibat" name="akibat" rows="3" required></textarea>
-                                </div>
-                                <div class="col-12 col-lg-6 form-group">
-                                    <label for="akarPenyebab">Akar Penyebab/Masalah</label>
-                                    <textarea class="form-control" id="akarPenyebab" name="akarPenyebab" rows="3" required></textarea>
-                                </div>
-                                <div class="col-12 col-lg-6 form-group">
-                                    <label for="rekomendasiDisepakati">Rekomendasi disepakati dengan audit</label>
-                                    <textarea class="form-control" id="rekomendasiDisepakati" name="rekomendasiDisepakati" rows="3" required></textarea>
-                                </div>
-                                <div class="col-12 col-lg-6 form-group">
-                                    <label for="tanggapanAuditi">Tanggapan Auditi</label>
-                                    <select id="dropdown" class="form-control" name="tanggapanAuditi" required>
-                                        <option value="">Tanggapan Auditi</option>
-                                        <option value="Setuju">Setuju</option>
-                                        <option value="Tidak Setuju">Tidak Setuju</option>
-                                    </select>
-                                </div>
-                                <div class="col-12 col-lg-6 form-group">
-                                    <label for="rencanaPerbaikan">Rencana Perbaikan</label>
-                                    <textarea class="form-control" id="rencanaPerbaikan" name="rencanaPerbaikan" rows="3" required></textarea>
-                                </div>
-                                <div class="col-12 col-lg-6 form-group">
-                                    <label for="jadwalPerbaikan" id="jadwalPerbaikan">Jadwal Perbaikan</label>
-                                    <input type="date" class="form-control" id="jadwalPerbaikan" name="jadwalPerbaikan" required>
-                                </div>
-                                <div class="col-12 col-lg-6 form-group">
-                                    <label for="rencanaPencegahan" id="rencanaPencegahan">Rencana Pencegahan</label>
-                                    <textarea class="form-control" id="rencanaPencegahan" rows="3" name="rencanaPencegahan" required></textarea>
-                                </div>
-                                <div class="col-12 col-lg-6 form-group">
-                                    <label for="jadwalPencegahan" id="jadwalPencegahan">Jadwal Pencegahan</label>
-                                    <input type="date" class="form-control" id="jadwalPencegahan" name="jadwalPencegahan" required>
-                                </div>
-                                <div class="col-12 mt-5">
-                                    <button type="submit" class="btn btn-primary">Simpan</button>
-                                </div>
-                            </form>
+                            <?php if ($value['sudah_terisi'] == true) { ?>
+                                <p>Deskripsi temuan pada <b>"<?= $value['deskripsi'] ?>"</b> pada kode kriteria <b><?= $value['kode_kriteria'] ?></b> sudah diisi, silakan cek pada <b>Kelola Deskripsi Temuan</b></p>
+                            <?php }else { ?>
+                                <form class="row text-center" action="/auditor/form-5/<?= $uuid2 ?>" method="post">
+                                    <?= csrf_field() ?>
+                                    <input type="text" name="id_ringkasan_temuan" value="<?= $value['id'] ?>" hidden>
+                                    <input type="text" name="penjaminMutuAudit" value="<?= $penjaminMutuAudit ?>" hidden>
+                                    <input type="text" name="pimpinanAuditi" value="<?= $wakil_auditi ?>" hidden>
+
+                                    <div class="col-12 col-lg-6 form-group">
+                                        <label for="deskripsiTemuan">Deskripsi Temuan</label>
+                                        <textarea class="form-control" id="deskripsiTemuan-visible-<?= $uniqueKey ?>" rows="3" disabled></textarea>
+                                        <textarea class="form-control hidden-textarea" id="deskripsiTemuan-<?= $uniqueKey ?>" name="deskripsiTemuan" rows="3" style="display: none;"></textarea>
+                                    </div>
+                                    <div class="col-12 col-lg-6 form-group">
+                                        <label for="kriteria">Kriteria</label>
+                                        <div class="form-control-static" id="kriteria-visible-<?= $uniqueKey ?>" style="height: auto; padding: 10px; background: #f1f1f1; border-radius: 5px;"></div>
+                                        <textarea class="form-control hidden-textarea" id="kriteria-<?= $uniqueKey ?>" name="kriteria" rows="3" style="display: none;"></textarea>
+                                    </div>
+                                    <div class="col-12 col-lg-6 form-group">
+                                        <label for="akibat">Akibat</label>
+                                        <textarea class="form-control" id="akibat" name="akibat" rows="3" required></textarea>
+                                    </div>
+                                    <div class="col-12 col-lg-6 form-group">
+                                        <label for="akarPenyebab">Akar Penyebab/Masalah</label>
+                                        <textarea class="form-control" id="akarPenyebab" name="akarPenyebab" rows="3" required></textarea>
+                                    </div>
+                                    <div class="col-12 col-lg-6 form-group">
+                                        <label for="rekomendasiDisepakati">Rekomendasi disepakati dengan audit</label>
+                                        <textarea class="form-control" id="rekomendasiDisepakati" name="rekomendasiDisepakati" rows="3" required></textarea>
+                                    </div>
+                                    <div class="col-12 col-lg-6 form-group">
+                                        <label for="tanggapanAuditi">Tanggapan Auditi</label>
+                                        <select id="dropdown" class="form-control" name="tanggapanAuditi" required>
+                                            <option value="">Tanggapan Auditi</option>
+                                            <option value="Setuju">Setuju</option>
+                                            <option value="Tidak Setuju">Tidak Setuju</option>
+                                        </select>
+                                    </div>
+                                    <div class="col-12 col-lg-6 form-group">
+                                        <label for="rencanaPerbaikan">Rencana Perbaikan</label>
+                                        <textarea class="form-control" id="rencanaPerbaikan" name="rencanaPerbaikan" rows="3" required></textarea>
+                                    </div>
+                                    <div class="col-12 col-lg-6 form-group">
+                                        <label for="jadwalPerbaikan" id="jadwalPerbaikan">Jadwal Perbaikan</label>
+                                        <input type="date" class="form-control" id="jadwalPerbaikan" name="jadwalPerbaikan" required>
+                                    </div>
+                                    <div class="col-12 col-lg-6 form-group">
+                                        <label for="rencanaPencegahan" id="rencanaPencegahan">Rencana Pencegahan</label>
+                                        <textarea class="form-control" id="rencanaPencegahan" rows="3" name="rencanaPencegahan" required></textarea>
+                                    </div>
+                                    <div class="col-12 col-lg-6 form-group">
+                                        <label for="jadwalPencegahan" id="jadwalPencegahan">Jadwal Pencegahan</label>
+                                        <input type="date" class="form-control" id="jadwalPencegahan" name="jadwalPencegahan" required>
+                                    </div>
+                                    <div class="col-12 mt-5">
+                                        <button type="submit" class="btn btn-primary">Simpan</button>
+                                    </div>
+                                </form>
+                            <?php } ?>
                         </div>
                     <?php } ?>
                 </div>
